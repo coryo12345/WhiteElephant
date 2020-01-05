3c65873c98607d4dc8fb4ee1adca44680e68f89d
coryo12345 <corydio12@gmail.com>
2020-01-04 21:45:26 -0500

    fix start script path for npm install

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 #install dependencies
-npm install --prefix ./backend
-npm install --prefix ./frontend
+npm install --prefix /root/whiteelephant/backend/
+npm install --prefix /root/whiteelephant/frontend/
 # start backend
 npm start --prefix /root/whiteelephant/backend/ &
 npm start --prefix /root/whiteelephant/frontend/
