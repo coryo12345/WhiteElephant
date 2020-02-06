f103b8b3c65e1cb49411ae37733022dc19805b94
coryo12345 <corydio12@gmail.com>
2020-02-05 19:07:42 -0500

    added restart policy

--- docker.sh
+++ docker.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-docker run -d -p 3100:3100 -v $(pwd):/root/whiteelephant --name whiteelephant node:10 /root/whiteelephant/start.sh
+docker run -d -p 3100:3100 -v $(pwd):/root/whiteelephant --name whiteelephant --restart always node:10 /root/whiteelephant/start.sh
